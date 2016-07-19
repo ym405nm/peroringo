8c5cbd3e2eb8fc4650a74f95a92cbc046ecb6c82
ym405nm <nsd08888@gmail.com>
2016-07-19 11:04:31 +0900

    出力の表記を若干修正

--- app/Http/Controllers/CveController.php
+++ app/Http/Controllers/CveController.php
@@ -13,32 +13,37 @@ use Goutte\Client;
 use Mockery\CountValidator\Exception;
 
 class CveController extends Controller {
-	public function api(Request $request){
-		$input_text = $request->input("text");
-		$input_user = $request->input("user_name");
+	public function api( Request $request ) {
+		$input_text = $request->input( "text" );
+		$input_user = $request->input( "user_name" );
 		// slackbot は無視
-		if("slackbot" === $input_user){
-			\Log::info("bot検知" . $input_text);
+		if ( "slackbot" === $input_user ) {
+			\Log::info( "bot検知" . $input_text );
+
 			return;
 		}
 		// CVE番号が含まれていないと無視
-		if(preg_match("/CVE-[0-9]{4}-[0-9]{4,7}/i", $input_text, $match)){
+		if ( preg_match( "/CVE-[0-9]{4}-[0-9]{4,7}/i", $input_text, $match ) ) {
 			$cve_number = $match[0];
-			$cve_number = strtoupper($cve_number); // 小文字で来たクエリを大文字に変換
-		}else{
-			\Log::info("該当記述なし" . $input_text);
+			$cve_number = strtoupper( $cve_number ); // 小文字で来たクエリを大文字に変換
+		} else {
+			\Log::info( "該当記述なし" . $input_text );
+
 			return;
 		}
-		$cve   = Cve::all();
-		$found = $cve->where( "cve", $cve_number );
-		if($found->count() != 0){
+		$mitre_url = sprintf( "https://cve.mitre.org/cgi-bin/cvename.cgi?name=%s", $cve_number );
+		$cve       = Cve::all();
+		$found     = $cve->where( "cve", $cve_number );
+		if ( $found->count() != 0 ) {
 			// CVEが見つかった場合
 			$description = $found->first()->description;
-			$url = $found->first()->url;
-			$text = $cve_number . " は「". $description . "」だよ （test）".$url;
-			return \Response::json( array("text" => $text) );
-		}else{
+			$url         = $found->first()->url;
+			$text        = $cve_number . " は「" . $description . "」だよ " . $url . "\n" . $mitre_url;
+
+			return \Response::json( array( "text" => $text ) );
+		} else {
 			// CVEが見つからなかった場合
+			// JVNを検索しにいく
 			try {
 				$client       = new Client();
 				$crawler      = $client->request( 'GET',
@@ -47,30 +52,32 @@ class CveController extends Controller {
 					return $node->attr( "href" );
 				} );
 				$search_array = preg_grep( "/contents/", $url_list );
-				if ( 0 < count($search_array) ) {
+				if ( 0 < count( $search_array ) ) {
 					// JNVから見つかった場合
 					$content_url = "http://jvndb.jvn.jp" . current( $search_array );
 					sleep( 1 );
 					$client     = new Client();
 					$crawler    = $client->request( 'GET', $content_url );
 					$page_title = $crawler->filter( "h2" )->last()->text();
-					\Log::info($page_title);
+					\Log::info( $page_title );
 
 					// ログをDBに保存する
-					$save_cve = new Cve;
-					$save_cve->cve = $cve_number;
+					$save_cve              = new Cve;
+					$save_cve->cve         = $cve_number;
 					$save_cve->description = $page_title;
-					$save_cve->url = $content_url;
+					$save_cve->url         = $content_url;
 					$save_cve->save();
 
-					$text = $cve_number . " は「". $page_title . "」だよ ".$content_url;
-					return \Response::json( array("text" => $text) );
+					$text = $cve_number . " は「" . $page_title . "」だよ " . $content_url;
+
+					return \Response::json( array( "text" => $text ) );
 
 				}
-			}catch(Exception $e){
-				\Log::error($e->getMessage());
+			} catch ( Exception $e ) {
+				\Log::error( $e->getMessage() );
 			}
-			return \Response::json(array("text" => $cve_number . " はJVNから見つからなかったよ。。。"));
+
+			return \Response::json( array( "text" => $cve_number . " はJVNから見つからなかったよ。。。\n" . $mitre_url ) );
 		}
 	}
 }
